20bdf18757c4124b06fb363bb34a30d51a7f4e3b
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-08-14 23:48:09 +0530

    Create string using pointers.c

---
+++ string using pointers.c
@@ -0,0 +1,14 @@
+// string using pointers
+#include<stdio.h>
+int main()
+{
+    char *str = "Welcome to the world of programming";
+    char *pstr;
+    pstr = str;
+    while(*pstr!= '\0')
+    {
+        printf("%c",*pstr);
+        pstr++;
+    }
+    return 0;
+}
